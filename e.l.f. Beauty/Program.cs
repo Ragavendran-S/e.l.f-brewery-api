6db214ea8e1354bdb98583265adb8bfc41411f9b
Ragavendran-S <ss.ragha@gmail.com>
2026-09-21 17:50:52 +0530

    test: isolate sqlite file for Testing env to avoid collisions

--- e.l.f. Beauty/Program.cs
+++ e.l.f. Beauty/Program.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 // configure services...
